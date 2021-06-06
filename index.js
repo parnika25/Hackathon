2ae150065ad2ab0a5e87926c3d91d4c17a567e5c
ShreyaDhir <dhirshreya@gmail.com>
2021-06-06 13:30:11 +0530

    Update: API error

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-
 console.log("This is my index js file");
 
 // Initialize the news api parameters
 let source = 'the-times-of-india';
-let apiKey = '672a19dd7a2b4868ad946df73d96a56a'
+let q = 'apple';
+let from = '2021-06-05';
+let to = '2021-06-05';
+let sortBy = 'popularity';
+let apiKey = 'dcfb23cac2704ea4af27c42a643bc3bc'
 
 // Grab the news container
 let newsAccordion = document.getElementById('newsAccordion');
 
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
+// xhr.open('GET', `https://newsapi.org/v2/everything?q=${q}&from=${from}&to=${to}&sortBy=${sortBy}&apiKey=${apiKey}`, '*', true);
 xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+// https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=dcfb23cac2704ea4af27c42a643bc3bc
+// https://newsapi.org/v2/everything?q=apple&from=2021-06-05&to=2021-06-05&sortBy=popularity&apiKey=dcfb23cac2704ea4af27c42a643bc3bc
 
 // What to do when response is ready
 xhr.onload = function () {
@@ -19,14 +25,14 @@ xhr.onload = function () {
         let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function (element, index) {
+        articles.forEach(function(element, index) {
             // console.log(element, index)
             let news = `<div class="card">
                             <div class="card-header" id="heading${index}">
                                 <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
                                     aria-expanded="false" aria-controls="collapse${index}">
-                                   <b>Breaking News ${index + 1}:</b> ${element["title"]}
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
                                 </button>
                                 </h2>
                             </div>
